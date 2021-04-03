79d9716a2acc925f587a89941fd3e17b4e98d080
roaraujo <roaraujo@student.42sp.org.br>
2021-04-03 20:50:19 +0000

    Agora sim exercicio cinco DONE

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,18 +6,9 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 04:50:55 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/04/03 20:34:25 by roaraujo         ###   ########.fr       */
+/*   Updated: 2021/04/03 20:46:09 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// Create a function that displays all different combinations of three different digits in
-// ascending order, listed by ascending order - yes, repetition is voluntary.
-// • Here’s the intended output :
-// $>./a.out | cat -e
-// 012, 013, 014, 015, 016, 017, 018, 019, 023, ..., 789$>
-// • 987 isn’t there because 789 already is.
-// • 999 isn’t there because the digit 9 is present more than once.
-// • Here’s how it should be prototyped :
 
 #include <unistd.h>
 
@@ -52,9 +43,4 @@ void	ft_print_comb(void)
 		}
 		centena++;
 	}
-	
-	write(1, "FINZAO", 6);
-	write(1, &centena, 1);
-	write(1, &dezena, 1);
-	write(1, &unidade, 1);
 }
