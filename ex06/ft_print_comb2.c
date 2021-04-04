7ef46da799feba838f5b354039ed3faffe66a0e0
roaraujo <roaraujo@student.42sp.org.br>
2021-04-04 13:33:25 +0000

    teste

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 04:51:29 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/04/03 19:43:04 by roaraujo         ###   ########.fr       */
+/*   Updated: 2021/04/03 22:45:35 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,47 @@
 // • Here’s how it should be prototyped :
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	ft_print_comb2(void)
 {
-	
+	char dezena_par_dir;
+	char unidade_par_dir;
+	char dezena_par_esq;
+	char unidade_par_esq;
+
+	// dezena_par_esq = '0';
+	// 
+	// while (dezena_par_esq <= '9')
+	// {
+	// 	unidade_par_esq = '0';
+	// 	while (unidade_par_esq <= '9')
+	// 	{
+	// 		dezena_par_dir
+	// 		write(1, &dezena_par_esq, 1);
+	// 		write(1, &unidade_par_esq, 1);
+	// 		write(1, " ", 1);
+	// 		unidade_par_esq++;
+	// 	}
+	// 	dezena_par_esq++;
+	// }
+
+	dezena_par_dir = '0';
+	bg
+	while (dezena_par_dir <= '9')
+	{
+		unidade_par_dir = '0';
+		while (unidade_par_dir <= '9')
+		{
+			dezena_par_dir
+			write(1, &dezena_par_dir, 1);
+			write(1, &unidade_par_dir, 1);
+			write(1, " ", 1);
+			unidade_par_dir++;
+		}
+		dezena_par_dir++;
+	}
+}
+
+int main(){
+	ft_print_comb2();
+	return 0;
 }
--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/03 04:52:31 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/04/03 19:42:59 by roaraujo         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   ft_putnbr.c										:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>	+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2021/04/03 04:52:31 by roaraujo		  #+#	#+#			 */
+/*   Updated: 2021/04/04 13:30:38 by roaraujo		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 // • Create a function that displays the number entered as a parameter. The function
@@ -17,8 +17,42 @@
 // ◦ ft_putnbr(42) displays "42"
 
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_putnbr(int nb)
 {
-
+	if(nb < 0)
+	{
+		write(1,"-",1);
+		nb = nb * (-1);
+	}
+	int teste;
+	int teste_mod;
+	int ao_contrario[5];
+	int loop;
+	int count;
+	
+	if(nb == 0)
+	{
+		write(1,"0",1);
+	}
+	else 
+	{
+		teste = 82567;
+		count = 4;
+	
+		while (teste > 1)
+		{
+			teste_mod = teste % 10;
+			ao_contrario[count] = teste_mod;
+			teste = teste / 10;	
+			count--;
+		}
+		loop = 0;	
+		while(loop < 5)
+		{
+			printf("%d", ao_contrario[loop]);
+			loop++;
+		}
+	}
 }
