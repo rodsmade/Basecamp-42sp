f6c31d0a507e64c69851e5c0a291e5efee0dafba
roaraujo <roaraujo@student.42sp.org.br>
2021-04-15 21:21:33 +0000

    Ex01 DONE

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/15 18:38:16 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/04/15 21:21:14 by roaraujo         ###   ########.fr       */
+/*   Created: 2021/04/15 18:39:36 by roaraujo          #+#    #+#             */
+/*   Updated: 2021/04/15 21:21:11 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-	int resultado;
-
-	resultado = nb;
 	if (nb < 0)
 		return (0);
-	if (nb == 0)
-		return (1);
-	while (--nb > 0)
-		resultado = resultado * nb;
-	return (resultado);
+	else if (nb == 0)
+		return (1); 
+	else
+		return nb * ft_recursive_factorial(nb - 1);
 }
